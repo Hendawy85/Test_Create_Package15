dd47f496a63499756fd5736fd232167fb3da35b3
Hendawy885 <Handawy9@yahoo.com>
2021-11-14 02:36:00 +0300

    egg

--- setup.py
+++ setup.py
@@ -11,16 +11,16 @@ setuptools.setup(
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Hendawy85/Test_Create_Package15",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/Hendawy85/Test_Create_Package15/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={ r"C:\Users\ali\PycharmProjects\Test_Create_Package\src" : "src"},
-    packages=setuptools.find_packages(where="src"),
+    package_dir={ r"C:\Users\ali\PycharmProjects\Test_Create_Package\src\Arthimatic_Package" :"Arthimatic_Package" },
+    packages=setuptools.find_packages(where="Arthimatic_Package"),
     python_requires=">=3.6",
 )
